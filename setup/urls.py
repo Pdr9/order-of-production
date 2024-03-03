06693e1c7ece6add89f8cf990810385ff5e421e1
pedro <pedro.neto.fpn@gmail.com>
2024-03-03 06:36:59 -0300

    adiçao de subdominio (mes) em cada op

--- setup/urls.py
+++ setup/urls.py
@@ -1,14 +1,18 @@
-from django.contrib import admin
 from django.urls import path, include
-from core.views import home, create
-from django.conf.urls.static import static
 from django.conf import settings
-from real_time_production.views import exibir_pagina
-
+from django.conf.urls.static import static
+from django.contrib import admin
+from core.views import create, exibir_pagina
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', include('core.urls')),
     path('create/', create, name='create'),
     path('pagina/', exibir_pagina, name='exibir_pagina'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+for month in range(1, 13):
+    urlpatterns += [
+        path(f'op/{month}/', include('core.urls')),
+    ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
